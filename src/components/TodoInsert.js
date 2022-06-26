cba67eb0099bdb4c2ecf8df4c6a8e02434c13080
Soyoung Choi <sydesign24@naver.com>
2022-06-26 23:10:06 +0900

    일정 관리 구현 및 디자인

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,36 @@
+import React, { useCallback, useState } from 'react'
+import { MdAdd } from 'react-icons/md'
+import '../styles/TodoInsert.scss'
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('')
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value)
+  }, [])
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault()
+
+      onInsert(value)
+      setValue('') // value 값 초기화
+    },
+    [onInsert, value],
+  )
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  )
+}
+
+export default TodoInsert
